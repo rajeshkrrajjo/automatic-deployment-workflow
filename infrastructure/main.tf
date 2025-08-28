12f733896cc196752b6320c9ac30b99d908372b0
Rajesh Kumar <rajeshkrrajjo@gmail.com>
2025-08-28 07:23:14 +0000

    fix: made changes for simulation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.1"
+    }
   }
 }
 
@@ -19,13 +23,20 @@ provider "github" {
   token = var.github_token
 }
 
+# Generate a unique suffix for storage account (required: must be globally unique)
+resource "random_string" "unique" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.environment}-${var.resource_group_name}"
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                      = "${var.environment}stor41223"
+  name                      = "${var.environment}stor${random_string.unique.result}"
   resource_group_name       = azurerm_resource_group.resource_group.name
   location                  = azurerm_resource_group.resource_group.location
   account_tier              = "Standard"
@@ -83,13 +94,15 @@ resource "azurerm_linux_function_app" "function_app" {
 resource "azurerm_static_site" "static_site" {
   name                = "${var.environment}-agd-static-site"
   resource_group_name = azurerm_resource_group.resource_group.name
-  location            = "westeurope"
+  location            = var.location
   sku_tier            = "Free"
   sku_size            = "Free"
 }
 
 resource "github_actions_secret" "update_site_api_key_secret" {
   repository      = "automatic-deployment-workflow"
-  secret_name     = "${var.environment}_api_key"
+  secret_name     = "${replace(var.environment, "-", "_")}_api_key"
   plaintext_value = azurerm_static_site.static_site.api_key
+
+  depends_on = [azurerm_static_site.static_site]
 }
